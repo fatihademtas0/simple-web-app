365f487ad76fd598935c53ca9ee7b32485cc20f9
fatihademtas0 <fatihademtas@outlook.com>
2024-10-19 21:26:33 +0300

    first

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyApp.Data;
+using MyApp.Models;
+
+namespace MyApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly MyAppContext _context;
+
+        public ItemsController(MyAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index ()
+        {
+            var item = await  _context.Items.ToListAsync();
+            return View(item);
+        }
+
+    }
+}
